dc76f8714d52805279b4d1db9cc9e2261844c294
MiguelRosales1264 <miguelrosales1264@gmail.com>
2025-01-09 10:37:04 -0700

    nav/footer now only needs to be edited in one place

---
+++ song-lyrics/frameLoader.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#footer-frame").load("../layout/footer.html");
+});
+
+// $(document).ready(function(){
+//     $("#navbar-frame").load("../layout/navbar.html");
+// });
