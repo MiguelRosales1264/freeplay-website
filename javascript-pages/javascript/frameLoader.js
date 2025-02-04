0a092576e4eae4031edb63a9428ae0150b4a8d7f
MiguelRosales1264 <miguelrosales1264@gmail.com>
2025-02-03 20:55:33 -0700

    Update Intro JS Page

---
+++ javascript-pages/javascript/frameLoader.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#footer-frame").load("../components/footer.html");
+});
+
+$(document).ready(function(){
+    $("#navbar-frame").load("../components/sub-navbar.html");
+});
