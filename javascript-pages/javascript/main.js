bfc8ab4be5879205941015225f35e5fa6e9f2686
MiguelRosales1264 <miguelrosales1264@gmail.com>
2025-03-16 07:55:25 -0600

    Added some JS

--- javascript-pages/javascript/main.js
+++ javascript-pages/javascript/main.js
@@ -1,7 +1,58 @@
 
-function myFunction(message) {
-    alert("This is a " + message + "!"); 
-    prompt("Enter your name:", "Name...");
+/* Some common event handlers
+- onclick
+- onmouseover
+- onmouseout
+- onfocus
+- onblur
+- onsubmit
+- onreset
+- onload
+- onunload
+- onresize
+- onscroll
+- onselect
+- onkeypress
+- onkeydown
+- onkeyup
+- onchange - for form fields */
+
+
+// attach onclick="myOnClick('message')" to any element
+function myOnClick(message) {
+    // alert("This is a " + message + "!"); 
+    // prompt("Enter your name:", "Name...");
+    console.log("This is a " + message + "!"); 
+}
+
+// attach onmouseover="myMouseOver('message')" to any element
+function myMouseOver(message) {
+    console.log("This is a " + message + "!"); 
+}
+
+// attach onload="loadFunction()" to the body tag
+function loadFunction() {
+    console.log("The page is loaded! " + new Date());
+}
+
+// attach onchange="myOnChange()" to any form field
+var firstName;
+function myOnChange() {
+    console.log("You changed the value of the input field!");
+    firstName = document.getElementById("inputField").value;
+    console.log("The new value is: " + firstName);
+}
+
+document.getElementById("butt").onclick = function() {
+    myOnClick("button");
+};
+
+document.getElementById("p1").onclick = function() {
+    myOnClick("paragraph");
+}
+
+document.getElementById("inputField").onchange = function() {
+    myOnChange();
 }
 
 
